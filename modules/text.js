15285e08432eb233e83c3fae22da6eeee4f5a5df
David C. Schedl <david.schedl@gmail.com>
2021-07-28 17:12:23 +0200

    Bounding Box Center can be used for the text

--- modules/text.js
+++ modules/text.js
@@ -7,13 +7,17 @@ const _v = /*@__PURE__*/ new Vector3();
 
 class Object3DAnnotation{
 
-    constructor( obj3D, text, camera  ) {
+    constructor( obj3D, text, camera, useGeometryBBCenter=false  ) {
         this.type = 'Object3DAnnotation';
         console.assert(obj3D instanceof Object3D, 'Error in Object3DAnnotation: obj3D is not a ThreeJS Object!');
         this.obj3D = obj3D;
         this.text = text;
         console.assert(camera instanceof Camera, 'Error in Object3DAnnotation: camera is not a ThreeJS Camera!');
         this.camera = camera;
+        this.useGBBC = useGeometryBBCenter; // obj3D has a geometry object!
+        if(this.useGBBC) {
+            console.assert('geometry' in obj3D, 'Error in Object3DAnnotation: obj3D does not have a geometry');
+        }
 
         // create html element
         const elem = document.createElement('div');
@@ -27,9 +31,19 @@ class Object3DAnnotation{
     // see: https://threejsfundamentals.org/threejs/lessons/threejs-align-html-elements-to-3d.html
     update() {
 
-        // get the position of the center of the cube
+        // update internal matrix:
         this.obj3D.updateWorldMatrix(true, false);
-        this.obj3D.getWorldPosition(_v);
+
+        if(!this.useGBBC){
+            // get the position of origin of the object
+            this.obj3D.getWorldPosition(_v);
+        }else{
+            // get the position of the center of the object's bounding box
+            const geometry = this.obj3D.geometry;
+            geometry.computeBoundingBox();
+            geometry.boundingBox.getCenter( _v );
+            this.obj3D.localToWorld( _v );
+        }
         
         // get the normalized screen coordinate of that position
         // x and y will be in the -1 to +1 range with x = -1 being
